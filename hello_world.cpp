01b9f72e6c00255ee03372b2769d4ab0d5fcebfb
AndrewB203 <bolshakov17419@gmail.com>
2024-04-21 12:56:01 +0300

    Translate comments to another language

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,9 @@
 ```here we can see the code without using namespace std;```
-
+Здесь мы используем ядро cтандартной библиотеки std;
 int main() {
-    string name;
-    cout << Enter your name: ;
-    cin >> name;
-    cout << Hello world from  << name << endl;
+    std::string name;
+    std::cout << Enter your name: ;
+    std::cin >> name;
+    std::cout << Hello world from  << name << endl;
     return 0;
 }
